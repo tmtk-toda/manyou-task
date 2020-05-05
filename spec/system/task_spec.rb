0b120127379d246ab48e084dd8e4f60b6296dfc0
tmtk <blindhurts.6001@gmail.com>
2020-05-05 11:52:12 +0900

    テスト修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'タスク管理機能', type: :system do
       # テストコードを it '~' do end ブロックの中に記載する
     it '作成済みのタスクが表示される' do
       # テストで使用するためのタスクを作成
-      task = FactoryBot.create(:task, name: 'task')
+      task = FactoryBot.create(:task, name: 'task', detail: 'detail')
       # タスク一覧ページに遷移
       visit tasks_path
       # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
       # have_contentされているか。（含まれているか。）ということをexpectする（確認・期待する）
-      expect(page).to have_content 'task'
+      expect(page).to have_content 'task' ,'detail'
       # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
     end
   end
@@ -20,20 +20,20 @@ RSpec.describe 'タスク管理機能', type: :system do
       # new_task_pathにvisitする（タスク登録ページに遷移する）
       # 1.ここにnew_task_pathにvisitする処理を書く
       visit new_task_path
-      task = Task.new(name: "task", detail: "task")
+      # task = Task.new(name: "", detail: "")
       # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
       # タスクのタイトルと内容をそれぞれfill_in（入力）する
       # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-      fill_in "task_name", with: task.name
+      fill_in "Name", with: 'name'
       # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-      fill_in "task_detail", with: task.detail
+      fill_in "Detail", with: 'detail'
       # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
       # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-      click_on 'Create Task'
+      click_on '登録する'
       # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
       # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
       # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-      expect(page).to have_content 'task'
+      expect(page).to have_content 'name' , 'detail'
       end
     end
   end
@@ -41,6 +41,7 @@ RSpec.describe 'タスク管理機能', type: :system do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示されたページに遷移する' do
          visit @show_task_path
+
 
      end
   end
