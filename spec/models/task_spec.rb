e510997f28ef58771838d2500430628fd2dec683
tmtk <blindhurts.6001@gmail.com>
2020-05-08 22:08:51 +0900

    バリデーションテスト実施

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
-
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe 'タスク管理機能', type: :system do
+  
+  before do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:task, name: '付け加えた名前１')
+    FactoryBot.create(:task, name: '付け加えた名前２')
+    FactoryBot.create(:second_task, name: '付け加えた名前３', detail: '付け加えたコンテント')
+    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
+    # 各テストで使用するタスクを1件作成する
+    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
+  end
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+    it 'titleが空ならバリデーションが通らない' do
+      task = Task.new(name: '', detail: '失敗テスト')
+      expect(task).not_to be_valid
+    end
+    end
+  end  
+    it 'contentが空ならバリデーションが通らない' do
+      # ここに内容を記載する
+      task = Task.new(name: '失敗テスと', detail: '')
+      expect(task).not_to be_valid
+    end
+    it 'nameとcontentに内容が記載されていればバリデーションが通る' do
+      # ここに内容を記載する
+      task = Task.new(name: '成功テスト', detail: '成功テスト')
+      expect(task).to be_valid
+    end
+  end
+   
