fdf2529be86871d2a35f455a0ba8e4b196df9257
tmtk <blindhurts.6001@gmail.com>
2020-05-13 00:20:28 +0900

    ステータス表示未完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -73,6 +73,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :detail, :deadline)
+      params.require(:task).permit(:name, :detail, :deadline, :completed)
     end
 end
